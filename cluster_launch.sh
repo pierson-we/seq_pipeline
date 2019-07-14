d8874fc4cca9e55c40d8fd09591ad02c14983c3c
pierson-we <pierson.we@gmail.com>
2019-07-14 15:36:24 -0700

    added launch script

---
+++ cluster_launch.sh
@@ -0,0 +1,6 @@
+module load CBC
+module load udocker
+
+udocker load -i /data/wpierson/seq_pipeline/pipeline_test_1.1.tar
+udocker create --name=seq_pipeline wpierson/pipeline_test:1.1
+udocker run --nosysdirs -v /data/wpierson/seq_pipeline/resources:/root/pipeline/resources -v /data/wpierson/seq_pipeline/code:/root/pipeline/code -v /data/wpierson/endometrioid:/root/endometrioid -v /home/wpierson/projects/seq_pipline:/root/seq_pipeline seq_pipeline /root/pipeline/code/test_launch.sh "$@"
