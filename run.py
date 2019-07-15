fd0fd441f5ef660ac622c6cf060030c3b4faee91
pierson-we <pierson.we@gmail.com>
2019-07-15 12:03:14 -0700

    .

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ def run_pipeline(args):
 
 	worker_scheduler_factory = luigi.interface._WorkerSchedulerFactory()
 
-	luigi.build([cases(sample_dict=sample_dict, project_dir=args.project_dir, sample_threads=sample_threads, cwd=os.getcwd())], resources={'threads': args.max_threads}, workers=args.workers, local_scheduler=args.local_scheduler, worker_scheduler_factory=worker_scheduler_factory) # , workers=args.workers #, scheduler_port=int(args.port)) # workers=sample_threads
+	luigi.build([cases(sample_dict=sample_dict, project_dir=args.project_dir, sample_threads=sample_threads, cwd=os.getcwd())], workers=args.workers, local_scheduler=args.local_scheduler, worker_scheduler_factory=worker_scheduler_factory) # , workers=args.workers #, scheduler_port=int(args.port)) # workers=sample_threads , resources={'threads': args.max_threads}
 
 class cases(luigi.Task):
 	# generated parameters
