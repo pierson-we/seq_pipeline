92b35dea6e1e0ba7defa548f480a529789e0a0d6
pierson-we <pierson.we@gmail.com>
2019-07-15 14:18:27 -0700

    .

--- run.py
+++ run.py
@@ -35,14 +35,14 @@ def run_pipeline(args):
 	else:
 		sample_threads = max(1, args.max_threads//len(sample_dict.keys()))
 
-	worker_scheduler_factory = luigi.interface._WorkerSchedulerFactory(resources = {'threads': args.max_threads})
+	worker_scheduler_factory = luigi.interface._WorkerSchedulerFactory()
 
 	# class resources(luigi.Config):
 	# 	threads = luigi.IntParameter(args.max_threads)
 
 	# resources()
 
-	luigi.build([cases(sample_dict=sample_dict, project_dir=args.project_dir, sample_threads=sample_threads, cwd=os.getcwd())], workers=args.workers, local_scheduler=args.local_scheduler, worker_scheduler_factory=worker_scheduler_factory) # , workers=args.workers #, scheduler_port=int(args.port)) # workers=sample_threads , resources={'threads': args.max_threads}
+	luigi.build([cases(sample_dict=sample_dict, project_dir=args.project_dir, sample_threads=sample_threads, cwd=os.getcwd())], workers=args.workers, local_scheduler=args.local_scheduler, worker_scheduler_factory=worker_scheduler_factory, override_defaults={'resources': {'threads': args.max_threads}}) # , workers=args.workers #, scheduler_port=int(args.port)) # workers=sample_threads , resources={'threads': args.max_threads}
 
 class cases(luigi.Task):
 	# generated parameters
