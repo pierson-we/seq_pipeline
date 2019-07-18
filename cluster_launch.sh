893a3aca925ca11b655602f0af4bd08f05604f66
pierson-we <pierson.we@gmail.com>
2019-07-17 17:37:05 -0700

    .

--- cluster_launch.sh
+++ cluster_launch.sh
@@ -13,12 +13,8 @@ l) LOCAL_SCHEDULER="-l";;
 esac
 done
 
-
 module load CBC
 module load python
 module load udocker
 
-# udocker load -i /data/wpierson/seq_pipeline/pipeline_test_1.1.tar
-# udocker create --name=seq_pipeline wpierson/pipeline_test:1.1
-echo "udocker run --novol=/etc/host.conf -v /data/wpierson/seq_pipeline/resources:/root/pipeline/resources -v /data/wpierson/seq_pipeline/code:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v /home/wpierson/projects/seq_pipeline:/root/seq_pipeline seq_pipeline /root/seq_pipeline/test_launch.sh -I /root/input -O /root -t ${THREADS} -s ${SAMPLE_THREADS} -w ${WORKERS} ${LOCAL_SCHEDULER}"
-#udocker run --novol=/etc/host.conf -v /data/wpierson/seq_pipeline/resources:/root/pipeline/resources -v /data/wpierson/seq_pipeline/code:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v /home/wpierson/projects/seq_pipeline:/root/seq_pipeline seq_pipeline /root/seq_pipeline/test_launch.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER 
+udocker run --novol=/etc/host.conf -v /data/wpierson/seq_pipeline/resources:/root/pipeline/resources -v /data/wpierson/seq_pipeline/code:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v /home/wpierson/projects/seq_pipeline:/root/seq_pipeline seq_pipeline /root/seq_pipeline/test_launch.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER 
