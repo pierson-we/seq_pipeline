0b92f581a46b4e36f49b1b109a322a9df08bc1d0
pierson-we <pierson.we@gmail.com>
2019-07-19 16:21:26 -0700

    .

--- snv_indel.py
+++ snv_indel.py
@@ -20,7 +20,11 @@ class mutect2_normal(luigi.Task):
 		return {'preprocess': preprocess.preprocess(case=self.case, sample=self.sample, cfg=self.cfg)}
 
 	def output(self):
-		return {'mutect2_normal': luigi.LocalTarget(os.path.join(self.cfg['output_dir'], self.case, 'variant_prep', '%s_%s_mutect2_normal.vcf.gz' % (self.case, self.sample))), 'err_log': luigi.LocalTarget(os.path.join(self.cfg['output_dir'], self.case, 'log', '%s_%s_mutect2_normal_err.txt' % (self.case, self.sample)))}
+		outputs =  {'mutect2_normal': luigi.LocalTarget(os.path.join(self.cfg['output_dir'], self.case, 'variant_prep', '%s_%s_mutect2_normal.vcf.gz' % (self.case, self.sample))), 'err_log': luigi.LocalTarget(os.path.join(self.cfg['output_dir'], self.case, 'log', '%s_%s_mutect2_normal_err.txt' % (self.case, self.sample)))}
+		for task in outputs:
+			if isinstance(outputs[task], luigi.LocalTarget):
+				outputs[task].mkdir()
+		return outputs
 
 	def run(self):
 		cmd = ['gatk4', 'Mutect2', '-R', self.cfg['fasta_file'], '--native-pair-hmm-threads', self.cfg['max_threads'], '-I', self.input()['preprocess']['bam'].path, '-O', self.output()['mutect2_normal'].path]
