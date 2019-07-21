16360f017c143bf884cfb23955c728f96c6df79c
pierson-we <pierson.we@gmail.com>
2019-07-21 14:46:12 -0700

    .

--- pipeline_utils.py
+++ pipeline_utils.py
@@ -72,7 +72,7 @@ def piped_command_call(cmds, err_log, output_file=False):
 
 def cluster_command_call(task, cmd, threads, ram, cfg, err_log=False, refresh_time=30):
 	cmd = [str(x) for x in cmd]
-	jobid, task_script_file, job_script_file = submit_job(cmd, threads, ram, cfg, task.task_id)
+	task.jobid, task.task_script_file, task.job_script_file = submit_job(cmd, threads, ram, cfg, task.task_id)
 	queue_start = time.time()
 	run_start = 0
 	while True:
