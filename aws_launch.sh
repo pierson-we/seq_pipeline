ed39b1af32f62e268c1d98d3cf0bc8cd35c705c5
pierson-we <pierson.we@gmail.com>
2019-07-25 16:12:07 -0700

    .

--- aws_launch.sh
+++ aws_launch.sh
@@ -1,20 +1,30 @@
-LOCAL_SCHEDULER=
+# LOCAL_SCHEDULER=
 
-while getopts I:O:R:C:S:t:s:w:l option
-do
-case "${option}"
-in
-I) INPUT_DIR=${OPTARG};;
-O) OUTPUT_DIR=${OPTARG};;
-R) RESOURCES_DIR=${OPTARG};;
-C) CODE_DIR=${OPTARG};;
-S) SEQ_PIPELINE_DIR=${OPTARG};;
-t) THREADS=${OPTARG};;
-s) SAMPLE_THREADS=$OPTARG;;
-w) WORKERS=$OPTARG;;
-l) LOCAL_SCHEDULER="-l";;
-esac
-done
+# while getopts I:O:R:C:S:t:s:w:l option
+# do
+# case "${option}"
+# in
+# I) INPUT_DIR=${OPTARG};;
+# O) OUTPUT_DIR=${OPTARG};;
+# R) RESOURCES_DIR=${OPTARG};;
+# C) CODE_DIR=${OPTARG};;
+# S) SEQ_PIPELINE_DIR=${OPTARG};;
+# t) THREADS=${OPTARG};;
+# s) SAMPLE_THREADS=$OPTARG;;
+# w) WORKERS=$OPTARG;;
+# l) LOCAL_SCHEDULER="-l";;
+# esac
+# done
+
+INPUT_DIR=~/input
+OUTPUT_DIR=~/
+RESOURCES_DIR=
+CODE_DIR=~/code
+SEQ_PIPELINE_DIR=~/seq_pipeline
+THREADS=96
+SAMPLE_THREADS=6
+WORKERS=50
+LOCAL_SCHEDULER=
 
 cd ~/seq_pipeline
 git pull
