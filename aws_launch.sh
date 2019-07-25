7ab03ba69ce02323803e3285e62be69a53a768e2
pierson-we <pierson.we@gmail.com>
2019-07-25 11:19:30 -0700

    .

--- aws_launch.sh
+++ aws_launch.sh
@@ -18,4 +18,4 @@ done
 
 # module load CBC python udocker
 # udocker run --novol=/etc/host.conf --novol=/etc/resolv.conf -v /data/wpierson/seq_pipeline/resources:/root/pipeline/resources -v /data/wpierson/seq_pipeline/code:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v /home/wpierson/projects/seq_pipeline:/root/seq_pipeline seq_pipeline /root/seq_pipeline/test_launch.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER module load CBC python udocker
-docker run -v ${RESOURCES_DIR}:/root/pipeline/resources -v ${CODE_DIR}:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v ${SEQ_PIPELINE_DIR}:/root/seq_pipeline seq_pipeline /root/seq_pipeline/test_launch.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER 
+docker run -v ${RESOURCES_DIR}:/root/pipeline/resources -v ${CODE_DIR}:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v ${SEQ_PIPELINE_DIR}:/root/seq_pipeline seq_pipeline /root/seq_pipeline/aws_pipeline.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER 
---
+++ aws_pipeline.sh
@@ -0,0 +1,5 @@
+. /root/.profile
+cd /root
+alias python=/usr/bin/python3.6
+alias python3=/usr/bin/python3.6
+export LUIGI_CONFIG_PATH=/root/seq_pipeline/luigi.cfg && python3.6 ./seq_pipeline/run.py "$@"
