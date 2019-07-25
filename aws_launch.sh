fa92d75937ffd7bf0f7df6381bcd4396d72a9941
pierson-we <pierson.we@gmail.com>
2019-07-25 16:19:58 -0700

    .

--- aws_launch.sh
+++ aws_launch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # LOCAL_SCHEDULER=
 
 # while getopts I:O:R:C:S:t:s:w:l option
@@ -17,8 +18,8 @@
 # done
 
 INPUT_DIR=~/input
-OUTPUT_DIR=~/
-RESOURCES_DIR=
+OUTPUT_DIR=~
+RESOURCES_DIR=~/resources
 CODE_DIR=~/code
 SEQ_PIPELINE_DIR=~/seq_pipeline
 THREADS=96
@@ -30,4 +31,4 @@ cd ~/seq_pipeline
 git pull
 # module load CBC python udocker
 # udocker run --novol=/etc/host.conf --novol=/etc/resolv.conf -v /data/wpierson/seq_pipeline/resources:/root/pipeline/resources -v /data/wpierson/seq_pipeline/code:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v /home/wpierson/projects/seq_pipeline:/root/seq_pipeline seq_pipeline /root/seq_pipeline/test_launch.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER module load CBC python udocker
-docker run -v ${RESOURCES_DIR}:/root/pipeline/resources -v ${CODE_DIR}:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v ${SEQ_PIPELINE_DIR}:/root/seq_pipeline seq_pipeline /root/seq_pipeline/aws_pipeline.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER 
+sudo docker run -v ${RESOURCES_DIR}:/root/pipeline/resources -v ${CODE_DIR}:/root/pipeline/code -v ${INPUT_DIR}:/root/input -v ${OUTPUT_DIR}/output:/root/output -v ${SEQ_PIPELINE_DIR}:/root/seq_pipeline seq_pipeline /root/seq_pipeline/aws_pipeline.sh -I /root/input -O /root -t $THREADS -s $SAMPLE_THREADS -w $WORKERS $LOCAL_SCHEDULER 
