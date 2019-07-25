2fbce64510bf4865fa4af8902f117faab68899ec
pierson-we <pierson.we@gmail.com>
2019-07-25 12:28:41 -0700

    .

--- run.py
+++ run.py
@@ -182,21 +182,23 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 	
-	# with open(os.getenv('LUIGI_CONFIG_PATH'), 'r') as f:
-	# 	config = f.read().split('[')
-	# new_config = []
-	# for section in config:
-	# 	if not section.startswith('resources]'):
-	# 		new_config.append(section)
-	# new_config = '['.join(new_config)
-	# while not new_config.endswith('\n\n'):
-	# 	new_config += '\n'
-	# new_config += '[resources]\nthreads=%s' % str(args.max_threads)
-	# with open(os.getenv('LUIGI_CONFIG_PATH'), 'w') as f:
-	# 	f.write(new_config)
+	with open(os.getenv('LUIGI_CONFIG_PATH'), 'r') as f:
+		config = f.read().split('[')
+	new_config = []
+	for section in config:
+		if not section.startswith('resources]'):
+			new_config.append(section)
+	new_config = '['.join(new_config)
+	while not new_config.endswith('\n\n'):
+		new_config += '\n'
+	new_config += '[resources]\nthreads=%s' % str(args.max_threads)
+	with open(os.getenv('LUIGI_CONFIG_PATH'), 'w') as f:
+		f.write(new_config)
 
 	run_pipeline(args)
 
+	with open(os.getenv('LUIGI_CONFIG_PATH'), 'w') as f:
+		f.write('['.join(config))
 	# luigi.build([bam_processing.cases(max_threads=args.max_threads, project_dir=args.project_dir, sample_dir=args.sample_dir, threads_per_sample=args.threads_per_sample, timestamp=timestamp)], workers=args.workers, local_scheduler=args.local_scheduler)
 
 	# luigi.build([bowtie(fastq_path=fastq_path, sam_path=sam_path, threads=threads, fasta_path=fasta_path), convert_bam(sam_path=sam_path, bam_path=bam_path)], workers=1, local_scheduler=False)
