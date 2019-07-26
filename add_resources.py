12eb3c3c3a5ef6fffc5559ba0cb79360fc6fa8ec
pierson-we <pierson.we@gmail.com>
2019-07-25 20:49:09 -0700

    .

--- add_resources.py
+++ add_resources.py
@@ -4,7 +4,8 @@ import os
 threads = sys.argv[1]
 
 if __name__ == '__main__':
-	with open(os.getenv('LUIGI_CONFIG_PATH'), 'r') as f:
+	# with open(os.getenv('LUIGI_CONFIG_PATH'), 'r') as f:
+	with open('luigi.cfg', 'r') as f:
 		config = f.read().split('[')
 	new_config = []
 	for section in config:
@@ -14,5 +15,6 @@ if __name__ == '__main__':
 	while not new_config.endswith('\n\n'):
 		new_config += '\n'
 	new_config += '[resources]\nthreads=%s' % str(threads)
-	with open(os.getenv('LUIGI_CONFIG_PATH'), 'w') as f:
+	# with open(os.getenv('LUIGI_CONFIG_PATH'), 'w') as f:
+	with open('luigi.cfg', 'w') as f:
 		f.write(new_config)
