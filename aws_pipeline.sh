96776dbbacc1c2954d1688b9810deab4e36109fd
pierson-we <pierson.we@gmail.com>
2019-07-25 19:53:14 -0700

    .

--- aws_pipeline.sh
+++ aws_pipeline.sh
@@ -3,4 +3,4 @@
 cd /root
 alias python=/usr/bin/python3.6
 alias python3=/usr/bin/python3.6
-export LUIGI_CONFIG_PATH=/root/seq_pipeline/luigi.cfg && python3.6 ./seq_pipelineadd_resources.py 96 && python3.6 ./seq_pipeline/run.py "$@"
+export LUIGI_CONFIG_PATH=/root/seq_pipeline/luigi.cfg && python3.6 ./seq_pipeline/add_resources.py 96 && python3.6 ./seq_pipeline/run.py "$@"
