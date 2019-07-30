378d65bb8042ab11d04ae1d72cf5f579776f2a13
pierson-we <pierson.we@gmail.com>
2019-07-29 19:38:47 -0700

    .

--- aws_pipeline.sh
+++ aws_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . /root/.profile
 cd /root
-apt-get -y install libapache-dbi-perl
+apt-get -y install libapache-dbi-perl libtry-tiny-perl
 alias python=/usr/bin/python3.6
 alias python3=/usr/bin/python3.6
 export LUIGI_CONFIG_PATH=/root/seq_pipeline/luigi.cfg
