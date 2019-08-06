ab8c10c93b6fa120f1d2e83039fe3ba5a308839c
pierson-we <pierson.we@gmail.com>
2019-08-05 20:19:56 -0700

    added facets

--- run.py
+++ run.py
@@ -153,7 +153,8 @@ class cases(luigi.Task):
 
 		executions = []
 		for case in self.sample_dict:
-			executions.append(snv_indel.variant_calling(cfg=cfg, case=case), cnv_sv.facets_snp_pileup(cfg=cfg, case=case))
+			executions.append(snv_indel.variant_calling(cfg=cfg, case=case))
+			executions.append(cnv_sv.facets_snp_pileup(cfg=cfg, case=case))
 
 		return executions
 
