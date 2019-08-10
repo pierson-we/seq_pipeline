0f13125f4cda31686868fd9f04e67f11f46761f1
pierson-we <pierson.we@gmail.com>
2019-08-09 22:33:36 -0700

    .

--- aws_pipeline.sh
+++ aws_pipeline.sh
@@ -2,9 +2,9 @@
 . /root/.profile
 apt-get -y install libapache-dbi-perl libtry-tiny-perl cmake
 cd /opt
-wget https://github.com/genome/bam-readcount/archive/v0.7.4.tar.gz && tar xvzf v0.7.4.tar.gz && rm -f v0.7.4.tar.gz
+git clone https://github.com/genome/bam-readcount.git
+cd bam-readcount && cmake -Wno-dev && make
 wget https://github.com/samtools/samtools/releases/download/1.2/samtools-1.2.tar.bz2 && tar -xvjf samtools-1.2.tar.bz2 && rm -f samtools-1.2.tar.bz2
-cd /opt/bam-readcount-0.7.4 && mkdir build && cd build && cmake ../ && make deps && make -j && make install
 cd /opt/samtools-1.2 && make -j && make install
 cd /root
 alias python=/usr/bin/python3.6
