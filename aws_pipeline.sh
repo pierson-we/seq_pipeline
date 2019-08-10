52c49c58033649b896145f13cf541dfd516fe68c
pierson-we <pierson.we@gmail.com>
2019-08-09 22:21:04 -0700

    .

--- aws_pipeline.sh
+++ aws_pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 . /root/.profile
+apt-get -y install libapache-dbi-perl libtry-tiny-perl cmake
 cd /opt
 wget https://github.com/genome/bam-readcount/archive/v0.7.4.tar.gz && tar xvzf v0.7.4.tar.gz && rm -f v0.7.4.tar.gz
 cd /opt/bam-readcount-0.7.4 && mkdir build && cd build && cmake ../ && make deps && make -j && make install
 cd /root
-apt-get -y install libapache-dbi-perl libtry-tiny-perl
 alias python=/usr/bin/python3.6
 alias python3=/usr/bin/python3.6
 export LUIGI_CONFIG_PATH=/root/seq_pipeline/luigi.cfg
